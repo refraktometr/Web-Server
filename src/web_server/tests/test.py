0d3f404f7b417c733fd4c9a2fa8ba8a56a8ef2c4
Filatov Andrey <Andfilatov@rambler.ru>
2014-08-17 23:03:50 +0300

    get rid off web_server

--- src/web_server/tests/test.py
+++
@@ -1,6 +0,0 @@
-name = 1
-value = 2
-expires =3
-
-cookie = '%s=%s; expires=%s; ' % (name, value, expires)
-print cookie
--- src/web_server/tests/test_db.py
+++
@@ -1,6 +0,0 @@
-import unittest2
-
-
-class TestCreateUser(unittest2.TestCase):
-    def test_success_create(self):
-        username = ''
