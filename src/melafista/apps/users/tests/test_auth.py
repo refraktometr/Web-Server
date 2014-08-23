999164b7c1c9756ad765e42b16cbae6d74640c8a
Filatov Andrey <Andfilatov@rambler.ru>
2014-08-23 11:05:57 +0300

    fixed

--- src/melafista/apps/users/tests/test_auth.py
+++ src/melafista/apps/users/tests/test_auth.py
@@ -2,3 +2,8 @@ from melafista.test_case import TestCase
 from apps.users.management import db as commands_db
 from apps.users import db as user_db
 
+
+class TestAuthorizeUser(TestCase):
+    def test_authorize_user(self):
+
+        authorize_user(response, user_id):
